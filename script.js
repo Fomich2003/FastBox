e22ce8c2f94ec5193eb30ecb0431394c2ab73548
Fomich2003 <fomichdima1a@gmail.com>
2024-10-18 17:33:27 +0300

    Add telegram bot part1

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ headernav.addEventListener('click',  (event) => {
     headernav.classList.remove('active')
    }
 })
+
+// Add func send to telegram
+
+let token = '7783405621:AAFUBel-SW9hh_LEt7jamAepPafQj_yZp5A'
+let chatId = '-1002380830440'
+let tgUrl = `https://api.telegram.org/bot${token}/sendMessage`
+
+let form = document.querySelector('#send-form')
+let formError = document.querySelector('.form-ok')
+let formOk = document.querySelector('.form-error')
+let inpName = document.querySelector('#inp-name')
+let inpPhone = document.querySelector('#inp-tel')
+let inpProduct = document.querySelector('#inp-url')
+let inpAddress = document.querySelector('#inp-address')
+
+
+async function sendData(message) {
+
+    return await fetch(tgUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: message,
+            parse_mode: "html"
+        })
+    })
+}
+
+//  sendData("Привіт Дмитро").then(Response => {
+//     if (Response.ok) {
+//         alert("Ваше повідомлення відправлено")
+//     } else {
+//         alert("Помилка")
+//     }
+// })
+ 
