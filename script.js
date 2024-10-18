6ab638e7b6a206f7e001b3af3595912aacc2937c
Fomich2003 <fomichdima1a@gmail.com>
2024-10-18 20:47:05 +0300

    End all website, add model, add telegram bot part 2

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
+// Control Body
+
+let body = document.body
+
+function fixBody() {
+    body.classList.add("fixed")
+}
+
+function unFixBody() {
+    isOpenMenu = false
+    body.classList.remove("fixed")
+}
+
+// Menu
 let burgerBtn = document.querySelector('.burger')
 let headernav = document.querySelector('header nav')
+let isOpenMenu = false
 
 burgerBtn.addEventListener('click', () => {
+    isOpenMenu = !isOpenMenu
     burgerBtn.classList.toggle('active')
     headernav.classList.toggle('active')
+    if (isOpenMenu) {
+        fixBody()
+    } else {
+        unFixBody()
+    }
 })
 
 headernav.addEventListener('click',  (event) => {
    if(event.target.closest('a')){
     burgerBtn.classList.remove('active')
     headernav.classList.remove('active')
+    unFixBody()
    }
 })
 
@@ -19,14 +41,43 @@ let token = '7783405621:AAFUBel-SW9hh_LEt7jamAepPafQj_yZp5A'
 let chatId = '-1002380830440'
 let tgUrl = `https://api.telegram.org/bot${token}/sendMessage`
 
-let form = document.querySelector('#send-form')
-let formError = document.querySelector('.form-ok')
-let formOk = document.querySelector('.form-error')
-let inpName = document.querySelector('#inp-name')
-let inpPhone = document.querySelector('#inp-tel')
-let inpProduct = document.querySelector('#inp-url')
-let inpAddress = document.querySelector('#inp-address')
+let form = document.querySelectorAll('#send-form')
+let formError = document.querySelectorAll('.form-error')
+let formOk = document.querySelectorAll('.form-ok')
+let formBtn = document.querySelectorAll('#send-form button')
 
+let inpName = document.querySelectorAll('#inp-name')
+let inpPhone = document.querySelectorAll('#inp-tel')
+let inpProduct = document.querySelectorAll('#inp-url')
+let inpAddress = document.querySelectorAll('#inp-address')
+
+form.forEach((singleForm, index) => {
+    singleForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        formBtn[index].disabled = true
+
+        let message = `Заявка з сайту FastBox\n\nІмя та Прізвище:\n${inpName[index].value}\n\nНомер телефону:\n${inpPhone[index].value}\n\nПосилання на товар:\n${inpProduct[index].value}\n\nАдреса доставки:\n${inpAddress[index].value}\n\n`
+    
+        sendData(message).then(response => {
+            if (response.ok) {
+                formOk[index].style.display = "block"
+            } else {
+                formError[index].style.display = "block"
+            }
+
+            setTimeout(() => {
+                inpName[index].value = ""
+                inpPhone[index].value = ""
+                inpProduct[index].value = ""
+                inpAddress[index].value = ""
+                formError[index].style.display = "none"
+                formOk[index].style.display = "none" 
+                formBtn[index].disabled = false
+            }, 2500)
+        })
+    })
+})
 
 async function sendData(message) {
 
@@ -41,11 +92,22 @@ async function sendData(message) {
     })
 }
 
-//  sendData("Привіт Дмитро").then(Response => {
-//     if (Response.ok) {
-//         alert("Ваше повідомлення відправлено")
-//     } else {
-//         alert("Помилка")
-//     }
-// })
- 
+// Add modal
+
+let modal = document.querySelector(".modal")
+let closeModal = document.querySelector(".modal__close-btn")
+let openModalBtns = document.querySelectorAll("#open-modal")
+
+openModalBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        modal.classList.add("active")
+        fixBody()
+    })
+})
+
+
+closeModal.addEventListener("click", () => {
+    modal.classList.remove("active")
+    unFixBody()
+})
+
